9b99eda177f1442c4dbbd2e2d50febf628f72562
CamRobinson <robinsoncameron1103@gmail.com>
2021-03-03 16:52:55 -0600

    Adjusted FleeBehavior

--- raygame/FleeBehavior.cpp
+++ raygame/FleeBehavior.cpp
@@ -5,7 +5,7 @@
 FleeBehavior::FleeBehavior()
 {
 	m_target = nullptr;
-	m_fleeForce = 1;
+	m_fleeForce = -1;
 }
 
 FleeBehavior::FleeBehavior(Actor* target, float seekForce)
@@ -18,7 +18,7 @@ MathLibrary::Vector2 FleeBehavior::calculateForce(Agent* agent)
 {
 	//Find the direction to move in
 	MathLibrary::Vector2 direction = MathLibrary::Vector2::normalize(m_target->getWorldPosition() - agent->getWorldPosition());
-	//Scale the direction vector by the seekForce
+	//Scale the direction vector by the fleeForce
 	MathLibrary::Vector2 desiredVelocity = direction * m_fleeForce;
 	//Subtract current velocity from desired velocity to find the steering force
 	MathLibrary::Vector2 steeringForce = desiredVelocity - agent->getVelocity();
